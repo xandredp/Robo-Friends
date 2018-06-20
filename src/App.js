61b84e9a15eb194e3c2f852b8c4a1dd45599ab84
Xandré Du Plooy <xduplooy@gmail.com>
2018-06-20 23:48:31 +1200

    Applying styles

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox'
 import {robots} from './robots';
+import './App.css'
 
 
 class App extends Component {
@@ -24,7 +25,7 @@ class App extends Component {
         })
         return (
             <div className='tc'>
-                <h1>Robo Friends</h1>
+                <h1 className='f1'>Robo Friends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
                 <CardList robots={filteredRobots}/>
             </div>
